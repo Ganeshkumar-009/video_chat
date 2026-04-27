9da2047a1e3f550f45b2f7b162c7a2a825bcd138
Ganeshkumar-009 <shannugannu6@gmail.com>
2026-04-27 20:56:15 +0530

    Fix: Enabled CORS for Socket.io to allow live site connections

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
 const cors = require('cors');
 app.use(cors());
