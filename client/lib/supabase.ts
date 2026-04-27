ebfc57543bf09cf9de2f92f4569183ef71ca2f0c
Ganeshkumar-009 <shannugannu6@gmail.com>
2026-04-27 19:40:49 +0530

    Config: Updated Supabase URL and Anon Key

--- client/lib/supabase.ts
+++ client/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://niqgrlklnpwkorfpxrln.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pcWdybGtsbnB3a29yZnB4cmxuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mzc0ODM3OSwiZXhwIjoyMDg5MzI0Mzc5fQ.AP4N1NPEzUPzmptZonMt5H7tKaBH_PwFePDeyLkOI1k';
+const supabaseUrl = 'https://jxaedtdahujzvyltbwci.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4YWVkdGRhaHVqenZ5bHRid2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3NTgzMDAsImV4cCI6MjA4OTMzNDMwMH0.NKdN2GvzAd2jo7bB_xiE0PPTL41BkdIORty1XBCZp2k';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
